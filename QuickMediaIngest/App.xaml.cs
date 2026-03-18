8adcd7fd95b9255f7f2c717231a793104cae017b
edwardlthompson <edwardleethompson@gmail.com>
2026-03-18 01:00:44 -0400

    Initial commit - Project Skeleton with Material Design

---
+++ QuickMediaIngest/App.xaml.cs
@@ -0,0 +1,8 @@
+using System.Windows;
+
+namespace QuickMediaIngest
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ QuickMediaIngest/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace QuickMediaIngest
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
